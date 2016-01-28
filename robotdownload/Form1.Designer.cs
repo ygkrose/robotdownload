a741f338f6b8d5005f6bacbeec2f2b77adc4b40f
ygkrose <ygkrose@gmail.com>
2016-01-28 16:19:03 +0800

    first upload

---
+++ robotdownload/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace robotdownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clearSuccessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearFailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 81);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(502, 222);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(321, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "http://playback.hointeractive.com:444/Asia/25-08-15/BJ/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "download URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "save to";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(321, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "D:\\download\\VIP BlackJack\\";
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(30, 331);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(500, 124);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(30, 482);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(500, 148);
+            this.listBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "success";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 467);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "fail";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearSuccessToolStripMenuItem,
+            this.clearFailToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 70);
+            // 
+            // clearSuccessToolStripMenuItem
+            // 
+            this.clearSuccessToolStripMenuItem.Name = "clearSuccessToolStripMenuItem";
+            this.clearSuccessToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearSuccessToolStripMenuItem.Text = "clear success";
+            this.clearSuccessToolStripMenuItem.Click += new System.EventHandler(this.clearSuccessToolStripMenuItem_Click);
+            // 
+            // clearFailToolStripMenuItem
+            // 
+            this.clearFailToolStripMenuItem.Name = "clearFailToolStripMenuItem";
+            this.clearFailToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearFailToolStripMenuItem.Text = "clear fail";
+            this.clearFailToolStripMenuItem.Click += new System.EventHandler(this.clearFailToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 644);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearSuccessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearFailToolStripMenuItem;
+    }
+}
+
---
+++ robotdownload/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace robotdownload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] aryString = (!String.IsNullOrEmpty(textBox1.Text.Trim())) ? textBox1.Lines : null;
+            string errid = "";
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                    client.DownloadDataCompleted += Client_DownloadDataCompleted;
+                    this.Cursor = Cursors.WaitCursor;
+                    foreach (string s in aryString)
+                    {
+                        if (string.IsNullOrEmpty(s)) continue;
+                        errid = s;
+                        
+                        string flvfile = textBox3.Text + s + ".flv";
+                        if (!Directory.Exists(textBox3.Text)) Directory.CreateDirectory(textBox3.Text);
+                        if (!File.Exists(flvfile))
+                        {
+                            //client.DownloadFileAsync(new Uri(textBox2.Text + s + ".flv"), flvfile);
+                            client.DownloadFile(textBox2.Text + s + ".flv", flvfile);
+                            listBox1.Items.Add(s);
+                        }
+                        else
+                        {
+                            listBox1.Items.Add(s);
+                        }
+                        textBox1.Text = textBox1.Text.Remove(textBox1.Text.IndexOf(s), s.Length+2);
+                    }
+
+                    textBox1.AppendText("\n");
+                    textBox1.AppendText("-------Finish!--------\n");
+                }
+                catch (Exception err)
+                {
+                    listBox2.Items.Add("Game ID:" + errid + ", ErrMsg:" + err.Message);
+                    if (textBox1.Text.Length <= errid.Length)
+                        textBox1.Text = textBox1.Text.Remove(textBox1.Text.IndexOf(errid), errid.Length);
+                    else
+                        textBox1.Text = textBox1.Text.Remove(textBox1.Text.IndexOf(errid), errid.Length + 2);
+                }
+                finally
+                {
+                    this.Cursor = Cursors.Default;
+                }
+                //client.DownloadFile("http://playback.hointeractive.com:444/Asia/25-08-15/BJ/2dpte5oizvvh8r2jt4hkzl0j.flv", "D:\\download\\a.flv");
+            }
+        }
+
+        private void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //progressBar1.Value = e.ProgressPercentage;
+            Application.DoEvents();
+        }
+
+        private void doDownLoadProcess()
+        {
+
+        }
+
+        private void clearSuccessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void clearFailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+    }
+}
